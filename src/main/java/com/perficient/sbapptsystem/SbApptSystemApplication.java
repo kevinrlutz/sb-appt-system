a69ec21fdf6d19c1fa363a85c120e4d8680933e4
kevinrlutz <kevinlutz429@gmail.com>
2022-06-28 15:59:37 -0400

    initial commit

---
+++ src/main/java/com/perficient/sbapptsystem/SbApptSystemApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.sbapptsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApptSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApptSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perficient/sbapptsystem/SbApptSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.sbapptsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApptSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
